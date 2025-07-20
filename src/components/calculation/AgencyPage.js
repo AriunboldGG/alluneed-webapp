df1b10cbb83ac710e81cedbd3b74b867e9ed2e46
ariunbold <programmuud@gmail.com>
2025-07-21 03:07:35 +0800

    updated agency page responsive

--- src/components/calculation/AgencyPage.js
+++ src/components/calculation/AgencyPage.js
@@ -115,7 +115,7 @@ const AgencyPage = ({ selectedCategory }) => {
 
   return (
     <div className="min-h-screen bg-gray-50 py-8">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="w-full px-4 sm:px-6 lg:px-8">
         {/* Header */}
         <div className="mb-8 flex items-center">
           <div className="w-6 h-6 mr-2 flex items-center justify-center">
